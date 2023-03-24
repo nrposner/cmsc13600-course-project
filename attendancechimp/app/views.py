a976cdc50089cc3954b6d66adaff69c14dc104e9
nrposner <100233813+nrposner@users.noreply.github.com>
2023-03-24 09:29:45 -0500

    fulfills step 7 on views

--- attendancechimp/app/views.py
+++ attendancechimp/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+message = "Welcome to our front end! We are Nicolas Posner and Carina Kane in CMSC 13600. Nico is a Data Science Major from Madrid, Spain. Carina does not have a bio yet."
 
 def index(request):
-    return render(request, 'app/index.html', {})
+    return render(request, 'app/index.html', {"message": message})
